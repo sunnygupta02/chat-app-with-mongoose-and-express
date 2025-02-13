1826f850e8291d74d043e6d942a5a6337c73c572
Sunny Gupta <sunnygupta4924@gmail.com>
2025-02-13 19:53:44 +0530

    developed chat model and defined schema for it

---
+++ models/chat.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+//defining schema
+const chatSchema=new mongoose.Schema({
+    from:{
+        type:String,
+        required:true
+    },
+    to:{
+        type:String,
+        required:true
+    },
+    msg:{
+        type:String,
+        
+    },
+    created_at:{
+        type:new Date(),
+        required:true
+    },
+
+
+})
+
+//creating model/collection
+
+const Chat=mongoose.model("Chat",chatSchema);
+module.exports=Chat;
